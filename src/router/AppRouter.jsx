30a7fe9c63ebf9a484235ec1d5eb9d2e99937125
adrian Estrada <aestradacolina@gmail.com>
2023-05-08 18:15:23 -0400

    segundo commit

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 
-import { LoginPage } from "../auth/pages/LoginPage";
+import { Navbar } from "../ui";
+
 import { MarvelPage } from "../heroes/pages/MarvelPage";
-import { DCPage } from "../heroes/pages/DCPage";
+import { DcPage } from "../heroes/pages/DCPage";
+import { LoginPage } from "../auth/pages/LoginPage";
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
+
       <Routes>
         <Route path="marvel" element={<MarvelPage />} />
-        <Route path="DC" element={<DCPage />} />
+        <Route path="DC" element={<DcPage />} />
 
         <Route path="login" element={<loginPage />} />
 
